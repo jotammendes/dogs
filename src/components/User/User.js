a45bca6bdd10149de87ab49002aeeb26419c3d9a
João Paulo Matos Mendes <64609444+jotammendes@users.noreply.github.com>
2021-07-18 20:53:43 -0300

    feat: criação de menu de usuário logado + responsividade

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import UserStats from './UserStats';
+import UserPhotoPost from './UserPhotoPost';
+import Feed from '../Feed/Feed';
+import { Routes, Route } from 'react-router-dom';
 
 const User = () => {
     return (
-        <div>
-            User
-        </div>
+        <section className="container">
+            <UserHeader />
+            <Routes>
+                <Route path="/" element={ <Feed /> } />
+                <Route path="/estatisticas" element={ <UserStats /> } />
+                <Route path="/postar" element={ <UserPhotoPost /> } />
+            </Routes>
+        </section>
     )
 }
 
