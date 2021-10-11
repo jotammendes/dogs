be67ef750b6d3eb1fe0982099c6d1ece6420f912
João Paulo Matos Mendes <64609444+jotammendes@users.noreply.github.com>
2021-10-11 18:12:05 -0300

    style: add estilos ao Footer

--- src/App.js
+++ src/App.js
@@ -13,23 +13,26 @@ import UserProfile from './components/User/UserProfile';
 import NotFound from './components/NotFound';
 
 function App() {
-    return <div>
-        <BrowserRouter>
-            <UserStorage>
-                <Header />
-
-                    <Routes>
-                        <Route path="/" element={ <Home /> } />
-                        <Route path="/login/*" element={ <Login /> } />
-                        <ProtectedRoute path="/conta/*" element={ <User /> } />
-                        <Route path="/foto/:id" element={ <Photo /> } />
-                        <Route path="/perfil/:user" element={ <UserProfile /> } />
-                        <Route path="*" element={ <NotFound /> } />
-                    </Routes>
-                <Footer />
-            </UserStorage>
-        </BrowserRouter>
-    </div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <UserStorage>
+                    <Header />
+                        <main className="AppBody">
+                            <Routes>
+                                <Route path="/" element={ <Home /> } />
+                                <Route path="/login/*" element={ <Login /> } />
+                                <ProtectedRoute path="/conta/*" element={ <User /> } />
+                                <Route path="/foto/:id" element={ <Photo /> } />
+                                <Route path="/perfil/:user" element={ <UserProfile /> } />
+                                <Route path="*" element={ <NotFound /> } />
+                            </Routes>
+                        </main>
+                    <Footer />
+                </UserStorage>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
