b7997937c438e75e73f249f68c0c0e5c8b76b7e2
João Paulo Matos Mendes <64609444+jotammendes@users.noreply.github.com>
2021-10-11 19:15:14 -0300

    feat: add gráficos da biblioteca Victory para Estatísticas + lazy loading em biblioteca

--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import { GET_STATS } from '../../Api';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" />
-            Estatísticas
-        </div>
-    )
+    const {data, loading, error, request} = useFetch();
+
+    React.useEffect(() => {
+        async function getData() {
+            const { url, options } = GET_STATS();
+            await request(url, options);
+        }
+        getData();
+    }, [request]);
+
+    if(loading) return <Loading />
+    if(error) return <Error error={error} />
+    if(data)
+        return (
+            <React.Suspense fallback={<div></div>}>
+                <Head title="Estatísticas" />
+                <UserStatsGraphs data={data} />
+            </React.Suspense>
+        );
+    else return null;
 }
 
 export default UserStats;
