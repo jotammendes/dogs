152d66ce7258cf55291dfd296edb0f0872c1805a
João Paulo Matos Mendes <64609444+jotammendes@users.noreply.github.com>
2021-10-11 12:22:05 -0300

    feature: add feed de usuário específico + página de erro 404

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Login from './components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './components/User/User';
 import ProtectedRoute from './components/Helper/ProtectedRoute';
+import UserProfile from './components/User/UserProfile';
+import NotFound from './components/NotFound';
 
 function App() {
     return <div>
@@ -20,7 +22,9 @@ function App() {
                         <Route path="/" element={ <Home /> } />
                         <Route path="/login/*" element={ <Login /> } />
                         <ProtectedRoute path="/conta/*" element={ <User /> } />
-                        <ProtectedRoute path="/foto/:id" element={ <Photo /> } />
+                        <Route path="/foto/:id" element={ <Photo /> } />
+                        <Route path="/perfil/:user" element={ <UserProfile /> } />
+                        <Route path="*" element={ <NotFound /> } />
                     </Routes>
                 <Footer />
             </UserStorage>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,6 +5,7 @@ import UserPhotoPost from './UserPhotoPost';
 import Feed from '../Feed/Feed';
 import { Routes, Route } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import NotFound from './components/NotFound';
 
 const User = () => {
     const {data} = React.useContext(UserContext);
@@ -16,6 +17,8 @@ const User = () => {
                 <Route path="/" element={ <Feed user={data.id} /> } />
                 <Route path="/estatisticas" element={ <UserStats /> } />
                 <Route path="/postar" element={ <UserPhotoPost /> } />
+                <Route path="*" element={ <NotFound /> } />
+
             </Routes>
         </section>
     )
